4b78c28be27c78d18e68bc37ce78933ad4e66ca0
Adit <aditshah2002@gmail.com>
2022-05-16 23:47:06 +0530

    Minor fixes

--- doctor/urls.py
+++ doctor/urls.py
@@ -6,5 +6,6 @@ from django.urls import path
 urlpatterns = [
     path('doctor_dash', views.doctor_dash, name='doctor_dash'),
     path('update_patient_data', views.update_patient_data, name='update_patient_data'),
-    path('view_patient_history', views.view_patient_history, name='view_patient_history')
+    path('view_patient_history', views.view_patient_history, name='view_patient_history'),
+    path('view_patient_history2', views.view_patient_history, name='view_patient_history2')
 ]
